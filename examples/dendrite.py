7cc761f149d7bb64e220f58d2168ca905878bd1a
adammaj1@o2.pl <adammaj1@o2.pl>
2018-11-06 22:00:29 +0100

    examples

---
+++ examples/dendrite.py
@@ -0,0 +1,23 @@
+# lami5.py
+#
+# Draws a dendrit lamination simply, with shaded gaps.
+
+import sys
+sys.path.append("..")
+import DrawLam
+from gmpy import mpq
+
+L = DrawLam.DrawLam()
+L.degree = 2
+depth = 10
+L.pullbackscheme = [(mpq(1,7), mpq(1,7)+mpq(2,7), True)]
+L.filename = "dendrite.png"
+L.start()
+
+gap = (mpq(1,7), mpq(2,7), mpq(4,7))
+L.thickness(2)
+L.setColor(0,0,0)
+L.setFill(0.8,0.8,0.8)
+L.iterative_preimages(gap, depth)
+L.writeout()
+print "Done!"
